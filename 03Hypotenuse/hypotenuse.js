bef08abc2483a82093dae614a4fcf80ed6a4e756
snegicodes <snegicodes@gmail.com>
2022-11-30 20:31:08 +0530

    fix: fixed result message

--- 03Hypotenuse/hypotenuse.js
+++ 03Hypotenuse/hypotenuse.js
@@ -2,9 +2,11 @@ const base = document.querySelector("#base-measure");
 const height = document.querySelector("#height-measure");
 const calcButton = document.querySelector("#calc-button");
 const result = document.querySelector("#result-hypo");
+const resultMsg = document.querySelector("#result-msg");
 
 calcButton.addEventListener("click", () => {
   const hypo = findHypotenuse(base.value, height.value);
+  resultMsg.style.display = "block";
   result.innerText = hypo;
 });
 
